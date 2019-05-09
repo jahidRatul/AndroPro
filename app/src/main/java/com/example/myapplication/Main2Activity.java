e7a2102fe14e18f8773bd2618bbcbab87405d21d
jahidRatul <jahidratul@gmail.com>
2019-05-09 15:58:45 +0600

    pic up working phase 1

--- app/src/main/java/com/example/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/myapplication/Main2Activity.java
@@ -1,6 +1,10 @@
 package com.example.myapplication;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.provider.MediaStore;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,21 +16,40 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Spinner;
 
+//imports for img upload
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.os.Environment;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+
+
 public class Main2Activity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-    String gender="male";
-    String agreement="Yes,Every information  is all true.";
+    String gender = "male";
+    String agreement = "Yes, Every information  is all true.";
+
+
+    private int REQUEST_CAMERA = 0, SELECT_FILE = 1;
+    private Button btnSelect;
+    private ImageView ivImage;
+    private String userChoosenTask;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        final EditText userName =  findViewById(R.id.placeName);
+        final EditText userName = findViewById(R.id.placeName);
         final EditText userEmail = findViewById(R.id.placeEmail);
         final Spinner spinner = findViewById(R.id.divisionSpinner);
-
-
 
 
         final Button button = findViewById(R.id.go3rdActivity);
@@ -34,22 +57,30 @@ public class Main2Activity extends AppCompatActivity implements AdapterView.OnIt
             public void onClick(View v) {
 
 
-                Intent intent=new Intent(Main2Activity.this,Main3Activity.class);
+                Intent intent = new Intent(Main2Activity.this, Main3Activity.class);
                 String NameValue = userName.getText().toString();
                 String EmailValue = userEmail.getText().toString();
                 String SpinnerText = spinner.getSelectedItem().toString();
-                intent.putExtra("Name",NameValue);
-                intent.putExtra("Email",EmailValue);
-                intent.putExtra("SpinnerText",SpinnerText);
-                intent.putExtra("checked",gender);
-                intent.putExtra("agreementCheck",agreement);
+                intent.putExtra("Name", NameValue);
+                intent.putExtra("Email", EmailValue);
+                intent.putExtra("SpinnerText", SpinnerText);
+                intent.putExtra("checked", gender);
+                intent.putExtra("agreementCheck", agreement);
 
                 startActivity(intent);
 
             }
         });
 
+        btnSelect = findViewById(R.id.btnSelectPhoto);
+        btnSelect.setOnClickListener(new OnClickListener() {
 
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
+        ivImage =  findViewById(R.id.ivImage);
 
 
 //       Spinner spinner = findViewById(R.id.genderSpinner);
@@ -81,14 +112,14 @@ public class Main2Activity extends AppCompatActivity implements AdapterView.OnIt
         boolean checked = ((RadioButton) view).isChecked();
 
         // Check which radio button was clicked
-        switch(view.getId()) {
+        switch (view.getId()) {
             case R.id.radio_male:
                 if (checked)
-                    gender="male";
+                    gender = "male";
                 break;
             case R.id.radio_female:
                 if (checked)
-                    gender="female";
+                    gender = "female";
                 break;
         }
     }
@@ -110,9 +141,117 @@ public class Main2Activity extends AppCompatActivity implements AdapterView.OnIt
 //        }
 
         if (checked)
-               agreement= "Yes,Every information  is all true.";
-            else
-               agreement="No,Check again please";
+            agreement = "Yes,Every information  is all true.";
+        else
+            agreement = "No,Check again please";
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case Utility.MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (userChoosenTask.equals("Take Photo"))
+                        cameraIntent();
+                    else if (userChoosenTask.equals("Choose from Library"))
+                        galleryIntent();
+                } else {
+                    //code for deny
+                }
+                break;
+        }
+    }
+
+    private void selectImage() {
+        final CharSequence[] items = {"Take Photo", "Choose from Library",
+                "Cancel"};
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(Main2Activity.this);
+        builder.setTitle("Add Photo!");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                boolean result = Utility.checkPermission(Main2Activity.this);
+
+                if (items[item].equals("Take Photo")) {
+                    userChoosenTask = "Take Photo";
+                    if (result)
+                        cameraIntent();
+
+                } else if (items[item].equals("Choose from Library")) {
+                    userChoosenTask = "Choose from Library";
+                    if (result)
+                        galleryIntent();
+
+                } else if (items[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+    private void galleryIntent() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);//
+        startActivityForResult(Intent.createChooser(intent, "Select File"), SELECT_FILE);
+    }
+
+    private void cameraIntent() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, REQUEST_CAMERA);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == SELECT_FILE)
+                onSelectFromGalleryResult(data);
+            else if (requestCode == REQUEST_CAMERA)
+                onCaptureImageResult(data);
+        }
+    }
+
+    private void onCaptureImageResult(Intent data) {
+        Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        thumbnail.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
+
+        File destination = new File(Environment.getExternalStorageDirectory(),
+                System.currentTimeMillis() + ".jpg");
+
+        FileOutputStream fo;
+        try {
+            destination.createNewFile();
+            fo = new FileOutputStream(destination);
+            fo.write(bytes.toByteArray());
+            fo.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        ivImage.setImageBitmap(thumbnail);
+    }
+
+    @SuppressWarnings("deprecation")
+    private void onSelectFromGalleryResult(Intent data) {
+
+        Bitmap bm = null;
+        if (data != null) {
+            try {
+                bm = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(), data.getData());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        ivImage.setImageBitmap(bm);
     }
 }
 
